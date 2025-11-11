43fe8198aed29ee8e8f450011cc5178e6536bb67
Khanh Chung <khanh.d.chung@gmail.com>
2025-11-10 23:28:44 -0800

    add SearchBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./utils/queryClient";
 import { AuthProvider } from "./contexts/AuthContext";
-import StatCard from "./components/common/StatCard";
+import SearchBar from "./components/common/SearchBar";
 
 const App = () => {
   return (
@@ -26,7 +26,7 @@ const App = () => {
                         fontWeight: "bold",
                       }}
                     >
-                      <StatCard label="hello" value="world" trend="up" />
+                      <SearchBar />
                     </div>
                   }
                 />
